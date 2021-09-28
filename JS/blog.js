67c9e99c8022d059b3abaf1ded096eec62c76859
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-09-28 22:35:18 +0200

    add

---
+++ JS/blog.js
@@ -0,0 +1,42 @@
+const baseUrl ='' ;
+const blogContainer = document.querySelector(".blog");
+
+async function getBlog(url) {
+  const response = await fetch(url);
+  const blog = await response.json();
+
+  for (let i = 0; i < blog.length; i++) {
+
+    if (i===3) {
+      break;
+    }
+    
+    blogContainer.innerHTML += `
+    <a href="specific.html?id=${blog[i].id}">
+      <div >
+        <div class='blog--posts-image'style="background-image:url('${blog[i]._embedded["wp:featuredmedia"]["0"].source_url}')"></div>
+        <span class='featuredmedia-text'><h4>${blog[i]._embedded["wp:featuredmedia"]["0"].title.rendered}</h4></span>
+        
+      </div>
+    </a>  `
+  }
+  
+}
+getBlog(baseUrl);
+
+
+
+const viewMoreTitle = document.querySelector(".blog-post-specific-title");
+
+const viewMoreContainer = document.querySelector(".blog-post-specific");
+
+const viewMoreButton = document.querySelector(".view-more");
+
+function viewMoreSection() {
+  viewMoreTitle.style.display ="flex";
+  viewMoreContainer.style.display ="flex";
+}
+
+viewMoreButton.onclick = viewMoreSection;
+
+
---
+++ JS/specific.js
@@ -0,0 +1,33 @@
+
+const querystring = document.location.search;
+const parms = new URLSearchParams(querystring);
+const id = parms.get("id");
+
+
+const url = "" +id;
+
+const blogSpecificContainer = document.querySelector(".blog-specific");
+
+async function blogSpecific() {
+  const response = await fetch(url);
+  const specific = await response.json();
+    console.log(specific);
+  for (let i = 0; i < specific.length; i++) {
+    
+    blogSpecificContainer.innerHTML += `
+  
+    <div>
+        
+      <div class='blog--specific-image'style="background-image:url('${specific[i].blog_post_layout_featured_media_urls.full[0]}')"></div>
+      <span class='featuredmedia-specific-text'><h4>${specific[i]["wp:featuredmedia"]["0"].title.rendered}</h4></span> 
+        
+    </div>
+  
+  
+  
+  `
+  }
+  
+ 
+}
+blogSpecific();
